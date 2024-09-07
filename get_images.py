de57487c300fa64243f29f078ed8c4cd949da9f5
Yoav Moratt <ymoratt@gmail.com>
2024-09-07 17:47:23 +0300

    first

--- get_images.py
+++ get_images.py
@@ -1,21 +1,79 @@
 import os
 import paramiko
+import time
+import cv2
+import datetime
 
-
-def get_image(server, username, password, localpath, remotepath):
+def open_connection(server, username, password):
   ssh = paramiko.SSHClient() 
   ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
   ssh.connect(server, username=username, password=password)
   sftp = ssh.open_sftp()
+  return ssh, sftp
+
+def close_connection(ssh, sftp):
+  sftp.close()  
+  ssh.close()
+  
+
+def get_image(sftp, localpath, remotepath):
   # sftp.put(localpath, remotepath)
   sftp.get(localpath=localpath, remotepath=remotepath)
-  sftp.close()
-  ssh.close()
+
+def del_remote_image(ssh_client, remotepath):
+  assert(remotepath.endswith('.jpg'))
+  remote_cmd = f'rm {remotepath}'
+  stdin, stdout, stderr = ssh_client.exec_command(remote_cmd)
+
+def get_image_list(ssh_client, remotepath):
+  image_list = []
+  remote_cmd = f'ls {remotepath}'
+
+
+  print(f'Remote cmd = <{remote_cmd}>')
+  stdin, stdout, stderr = ssh_client.exec_command(remote_cmd)
+  # We iterate over stdout
+  for line in stdout:
+    line = line.strip()
+#    print('<-' + line + '->')  
+    if line.endswith('.jpg'):
+#      print('... ' + line)  
+      image_list.append(line)
+  return image_list
+  
+
+
+def get_images_iteration(ssh, sftp, remotepath, localpath):
+  image_names = get_image_list(ssh, remotepath)
+ 
+  #wait for the last image to be fully written
+  time.sleep(3)  
+  for img_name in image_names:
+    remote_img_path = f'{remotepath}/{img_name}'
+    local_img_path  = f'{localpath}/{int(datetime.datetime.now().timestamp())}_{img_name}'
+    print(f'image_paths = {remote_img_path} --> {local_img_path}')
+    get_image(sftp, localpath=local_img_path, remotepath=remote_img_path)
+    del_remote_image(ssh, remote_img_path)
+
+
 
 
 if __name__ == "__main__":
-  print('Yey')
+  remotepath='Documents/yoav/images'
+  localpath=r'c:\temp'
+  server='moratt'
+  username='ymoratt'
+  password='Sheshet7'
 
-  get_image(server='moratt',username='ymoratt', password='Sheshet7',localpath=r'c:\temp\image_15.jpg', remotepath='Documents/yoav/images/image_15.jpg')
+  print(f'getting images from {username}@{server}:{remotepath}')
+  ssh, sftp = open_connection(server='moratt',username='ymoratt', password=password)
+  
+  for i in range(0,1000):
+    print(f'Iteration {i}')
+    get_images_iteration(ssh=ssh, sftp=sftp, remotepath=remotepath, localpath=localpath)
+    time.sleep(10)
+
+
+  close_connection(ssh, sftp)
 
   print('End')
---
+++ id_a_cat.py
@@ -0,0 +1,97 @@
+import os
+import tensorflow as tf
+from argparse import ArgumentParser
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+ 
+
+def detect_cat(image_path, model, num_tags):
+
+    # Load and preprocess the image
+    img = image.load_img(image_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+
+    # Make predictions
+    predictions = model.predict(x)
+    decoded_predictions = decode_predictions(predictions, top=num_tags)[0]
+
+
+    # Check if 'cat' is in the top 3 predictions
+    for _, label, score in decoded_predictions:
+#        print(f'<== label is {label}')
+        if 'cat' in label.lower():
+            return True, f"Cat detected with confidence {score:.2f} in image {image_path}"
+
+    return False, f"No cat detected in the image {image_path}"
+
+
+def detect_cat_in_folder(images_root, num_tags):
+  ratio = 0.0
+  num_images = 0
+  num_cat_images = 0
+  # Load the MobileNetV2 model pre-trained on ImageNet
+  model = MobileNetV2(weights='imagenet') 
+  print(f'looking for pics in  {images_root} ')
+
+  for img_name in os.listdir(images_root):
+#    print(f'analyzing {img_name} ')
+    if img_name.endswith('.jpg'):      
+      img_path = os.path.join(images_root,img_name)
+      has_cat, message = detect_cat(img_path, model, num_tags)
+      num_images += 1
+      if (has_cat):
+        num_cat_images += 1
+        print(message)
+
+  return float(num_cat_images)/float(num_images)
+    
+
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("--pic_path", 
+                        default=os.path.join('c:' ,'temp'), 
+                        help="path to where the pictures are stored")
+
+    args = parser.parse_args()
+    if os.path.isdir(args.pic_path):
+        args.pic_path = os.path.abspath(args.pic_path)
+    else:
+        raise ValueError('ERROR: Can\'t find ' + args.pic_path)
+    return args
+
+
+args = parse_arguments()
+
+# Example usage
+images_root = args.pic_path
+cat_res = {}
+for cat_root in os.listdir(images_root):
+  cat_res[cat_root] = {}
+  num_tags = 0
+  ratio = 0.0
+  while ratio < 100.0:
+    num_tags += 30
+    ratio = detect_cat_in_folder(images_root, num_tags)
+    cat_res[cat_root][num_tags] = ratio
+
+for c in cat_res:
+   print(cat_res[c])
+     
+   
+
+
+
+ 
+
+ 
+
+ 
+
+ 
